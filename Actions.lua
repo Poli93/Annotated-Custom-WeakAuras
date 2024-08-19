c9f7b41677e6634751908f801fe111d5effc8c6f
Idan <tjyeee@gmail.com>
2024-08-19 22:17:32 +0300

    Create Actions.lua

---
+++ Actions.lua
@@ -0,0 +1,46 @@
+-- Actions Custom OnInit
+--[[
+• No `function() ... end` required. 
+• Nothing sent in. 
+• No return expected. 
+
+This is simply an area that can be used to run some code.
+
+RUNS ONCE WHEN THE AURA IS FIRST LOADED THEN NEVER AGAIN UNTIL A UI RELOAD OR WA CONFIG OPENED AND CHANGES MADE TO THIS CODE BLOCK
+
+This code block is exceptionally usaful for creating settings, or tables of info, or functions. Create vars/objects like this in `aura_env` and they will load only once but can be used throughout your Aura.  
+
+Wiki: https://github.com/WeakAuras/WeakAuras2/wiki/Custom-Actions
+]]
+
+-- Actions Custom OnShow
+--[[
+• No `function() ... end` required. 
+• Nothing sent in. 
+• No return expected. 
+
+This is simply an area that can be used to run some code.
+
+RUNS ONCE AS THE AURA IS TRIGGERED (SHOWN). TO TRIGGER AGAIN THE AURA MUST COMPLETELY UNTRIGGER THEN RETRIGGER.
+
+Wiki: https://github.com/WeakAuras/WeakAuras2/wiki/Custom-Actions
+]]
+
+-- Actions Custom OnHide
+--[[
+• No `function() ... end` required. 
+• Nothing sent in. 
+• No return expected. 
+
+This is simply an area that can be used to run some code.
+
+RUNS ONCE AS THE AURA IS HIDDEN (UNTRIGGERED). TO RUN THIS CODE AGAIN THE AURA MUST TRIGGER THEN UNTRIGGER AGAIN.
+
+Wiki: https://github.com/WeakAuras/WeakAuras2/wiki/Custom-Actions
+]]
+
+
+
+
+
+

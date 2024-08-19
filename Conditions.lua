7feea723b3d3445f6262f860d6f5b32997339f55
Idan <tjyeee@gmail.com>
2024-08-19 22:16:47 +0300

    Create Conditions.lua

---
+++ Conditions.lua
@@ -0,0 +1,22 @@
+-- If Trigger1 = True Then Run Custom Code:
+--[[
+No "function() ... end" required. 
+Nothing send in. 
+No return expected. 
+
+This is simply an area that can be used to run some code.
+
+RUNS ONCE AS THE CONDITION IS PASSED
+]]
+
+-- If Trigger2 = True Then Run Custom Code:
+--[[
+No "function() ... end" required. 
+Nothing send in. 
+No return expected. 
+
+This is simply an area that can be used to run some code.
+
+RUNS ONCE AS THE CONDITION IS PASSED
+]]
+

aba2f0dcf6fd4bce4ada229d43026dc69fa33557
Idan <tjyeee@gmail.com>
2024-08-19 22:16:00 +0300

    Create Display.lua

---
+++ Display.lua
@@ -0,0 +1,46 @@
+--[[
+if using a Timed duration info then:
+    function(expirationTime, duration, progress, formatedDuration, name, icon, stacks) 
+    • "progress" is a formatted string and shouldn't be used for arithmetic
+    • "duration" is simply the number of seconds
+    • "formatedDuration" is a formatted string showing minutes if applicable
+
+if using a Static duration info then:
+    function(total, value, value, total, name, icon, stacks)
+    
+• "icon" is a formatted string with escape sequence setup to display the texture
+
+Expected return:
+    return text (string)
+• Can return nil
+
+Wiki: https://github.com/WeakAuras/WeakAuras2/wiki/Text-Replacements
+]]
+
+function() 
+    
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
